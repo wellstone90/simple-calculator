6b2a222d71ada3432e867c0536e613eebf379106
wellstone90 <wellstone90@naver.com>
2016-08-24 15:41:50 +0900

    add dec func to Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,5 +1,8 @@
 public class Calc {
-	public int add(int x,int y){//add method
+	
+	private int r = 0;
+	
+	public int add(int x, int y){//add method
 		return x+y;
 	}
 
@@ -9,5 +12,9 @@ public class Calc {
 
 	public int mul(int x, int y){//multiplication method 1 2
 		return x*y;
+	}
+	
+	public void dec(int d){ //add method
+		r = r-d;
 	}
 }
